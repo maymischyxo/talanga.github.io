a5f5c420b6495a2e5e645a85a4837e8d3cddfc8e
maymischyxo <maymischyxo@gmail.com>
2024-06-20 19:02:16 +0100

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+let corp = document.querySelector("header");
+corp.addEventListener("click", () => {
+  console.log("le header a été cliqué");
+  corp.classList.toggle("ef_header");
+});
+
+// corp.style.background = "yellow";
+// alert("Mbotein!");
+// console.log("zbazoba");
